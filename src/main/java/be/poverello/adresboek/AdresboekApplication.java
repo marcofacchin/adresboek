a310eff7ced421cdbc652eddb965c96ad68be89a
MarcoFacchin <marco.facchin@vdabcampus.be>
2025-05-11 19:55:32 +0200

    folders opschonen

---
+++ src/main/java/be/poverello/adresboek/AdresboekApplication.java
@@ -0,0 +1,13 @@
+package be.poverello.adresboek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdresboekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdresboekApplication.class, args);
+    }
+
+}
